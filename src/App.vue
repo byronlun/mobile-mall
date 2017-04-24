442c1bf25a5a9388327a1875423bba0475d1011a
byronlun <2664181855@qq.com>
2017-04-24 20:56:34 +0800

    添加sass语法配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
     <div id="app">
-        <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
-			<router-link to="/" slot="left">
-				<mt-button icon="back" @click="handleClose">返回</mt-button>
-			</router-link>
-			<mt-button icon="more" slot="right"></mt-button>
-		</mt-header>
+        <mt-header title="微商城"></mt-header>
 		<mt-tabbar v-model="selected">
 			<mt-tab-item id="home">
 				<img slot="icon" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==">
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <h1>{{ msg }}</h1>
+    <mt-swipe class="home-swipe" :auto="2000">
+      <mt-swipe-item v-for="(item, index) in slideArr" :key="item+index">
+				<img :src="item" />
+	 		</mt-swipe-item>
+    </mt-swipe>
   </div>
 </template>
 
@@ -9,9 +13,33 @@ export default {
   name: 'home',
   data () {
     return {
-      msg: 'home'
+      msg: 'home',
+	  slideArr: ['./static/image/0.jpg', './static/image/1.jpg']
+    }
+  },
+  methods: {
+    handleChange() {
+      console.log(1)
     }
   }
 }
 </script>
-  
+<style  lang="scss">
+.home {
+	.home-swipe {
+		height: 160px;
+		color: #fff;
+		font-size: 30px;
+		text-align: center;
+		margin-bottom: 20px;
+		.mint-swipe-item {
+			line-height: 160px;
+			height: 160px;
+			img {
+				width: 100%;
+			}
+		}
+	}
+}
+
+</style>
