df2efc1a9f2a7da8cc4a2e4333d44bea6234ce67
byronlun <byronlunfang@gmail.com>
2017-04-26 21:25:59 +0800

    使用vue-resources来请求数据，暂时使用easy-mock实现模拟数据

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,6 +5,7 @@
 				<img :src="item" />
 	 		</mt-swipe-item>
     </mt-swipe>
+
   </div>
 </template>
 
@@ -14,9 +15,16 @@ export default {
   data () {
     return {
       msg: 'home',
-	  slideArr: ['./static/image/0.jpg', './static/image/1.jpg']
+	  	slideArr: []
     }
   },
+	beforeCreate() {
+		// 这里使用www.easy-mock.com这个网站提供的模拟数据接口哈哈
+		this.$http.get('https://www.easy-mock.com/mock/59009906875d7232a38b23ca/example/home/getSlidePhoto').then(response => {
+			console.log(response)
+			this.slideArr = response.body
+		})
+	},
   methods: {
     handleChange() {
       console.log(1)
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 Vue.use(MintUI)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
